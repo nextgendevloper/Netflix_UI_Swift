85e01970c17fea895e0ccbf09ec8e0cefb0f2a84
hkBeast <himanshu.singh@simplyentertaining.us>
2022-11-25 10:45:45 +0530

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by zmobile on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
